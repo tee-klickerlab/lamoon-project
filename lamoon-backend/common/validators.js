a5fc481c4eb73ec816ce17d6b374bdb23edf4718
tee-klickerlab <97880543+tee-klickerlab@users.noreply.github.com>
2022-06-25 13:33:46 +0700

    Revert "Revert "Merge pull request #3 from TeeDevTool/development""
    This reverts commit f8802acab146d58e269e4e7189db119d9d95534d.

--- lamoon-backend/common/validators.js
+++ lamoon-backend/common/validators.js
@@ -1,5 +1,10 @@
 const { check, param } = require("express-validator");
 
+const reqParams = [
+  // validate params, id is required, id must be number type.
+  param("id").trim().isNumeric().withMessage("id must be a number"),
+];
+
 const addMenu = [
   // menu is required, length is less than 100 characters.
   check("menu").trim().notEmpty().isLength({ max: 100 }),
@@ -9,20 +14,19 @@ const addMenu = [
   check("sale").trim().isNumeric().withMessage("sale must be a number"),
 ];
 
-const updateMenu = [
+const updateMenu = addMenu.concat(reqParams);
+
+const addReport = [
   // menu is required, length is less than 100 characters.
-  check("menu").trim().notEmpty().isLength({ max: 100 }),
-  // cost is required, cost must be number type.
-  check("cost").trim().isNumeric().withMessage("cost must be a number"),
-  // sale is required, sale must be number type.
-  check("sale").trim().isNumeric().withMessage("sale must be a number"),
-  // validate params, id is required, id must be number type.
-  param("id").trim().isNumeric().withMessage("sale must be a number"),
+  check("name").trim().notEmpty().isLength({ max: 100 }),
+  // sum_cost is required, sum_cost must be number type.
+  check("sum_cost").trim().isNumeric().withMessage("sum_cost must be a number"),
+  // sum_sale is required, sum_sale must be number type.
+  check("sum_sale").trim().isNumeric().withMessage("sum_sale must be a number"),
+  // menu_id is required, menu_id must be array of number.
+  check("menu_id.*").isNumeric().withMessage("menu_id must be array of number"),
 ];
 
-const reqParams = [
-  // validate params, id is required, id must be number type.
-  param("id").trim().isNumeric().withMessage("sale must be a number"),
-];
+const updateReport = addReport.concat(reqParams);
 
-module.exports = { addMenu, updateMenu, reqParams };
+module.exports = { addMenu, updateMenu, reqParams, addReport, updateReport };
